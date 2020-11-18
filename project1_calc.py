158ea1bb1459073aa7531ccf845ccf16ca88a617
Marie Dias <mariane.gomes@fisica.ufmt.br>
2020-11-18 16:29:19 -0400

    run global

---
+++ project1_calc.py
@@ -0,0 +1,27 @@
+#Projeto 1: Calculadora
+import re
+
+print("CALCULADORA DA DISCÓRIDA")
+print("por Marie Dias.\n")
+
+print("Digite 'parar' para sair\n")
+prev = 0
+run = True
+
+
+def performMath():
+    global run
+    # Estamos modificando a variável run apenas dentro da função
+    # Se deixarmos assim, mesmo que o usuário escolha sair da calculadora, a execução não será finalizada,
+    # pois fora da função a variável booleana run ainda recebe o valor True
+    # Assim, ao tornar run global, mesmo que a modifiquemos dentro da func, este valor será modif globalmente
+    
+    equation = input("Digite a equação: ")
+    if equation == 'parar':
+        run= False
+    else:
+        print("Você digitou:", equation)
+
+
+while run:
+    performMath()
