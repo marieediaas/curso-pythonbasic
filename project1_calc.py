3a902a6c989af2796a4f6659bc33d277a871aba6
Marie Dias <mariane.gomes@fisica.ufmt.br>
2020-11-18 17:19:40 -0400

    parte final

--- project1_calc.py
+++ project1_calc.py
@@ -10,22 +10,33 @@ run = True
 
 
 def performMath():
+    global prev
     global run
     # Estamos modificando a variável run apenas dentro da função.
     # Se deixarmos assim, mesmo que o usuário escolha sair da calculadora, a execução não será finalizada,
     # pois fora da função a variável booleana run ainda recebe o valor True.
     # Assim, ao tornar run global, mesmo que a modifiquemos dentro da func, este valor será modif globalmente.
-    
-    equation = input("Digite a equação: ")
+
+    equation = ""
+    if prev == 0:
+        equation = input("Digite a equação: ")
+    else:
+        equation = input(str(prev))
+
+
     if equation == 'parar':
+        print("Tchau!")
         run= False
     else:
         equation = re.sub('[a-zA-Z,.:()" "]','',equation)
         #a variável terá apenas valores que excluam os caracteres entre os colchetes
         #assim, previne-se algum erro que possa surgir pelo uso da built in eval
-        prev = eval(equation)
-        #evaluate permite que o programa execute um código python "dentro de si"
-        print("Você digitou:", prev)
+
+        if prev==0:
+            prev = eval(equation)
+            #evaluate permite que o programa execute um código python "dentro de si"
+        else:
+            prev = eval(str(prev) + equation)
 
 
 while run:
