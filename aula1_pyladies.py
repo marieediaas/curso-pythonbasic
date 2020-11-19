626b0ba255b67dc3f392d1570cad4548f85b4200
Marie Dias <mariane.gomes@fisica.ufmt.br>
2020-11-19 12:37:59 -0400

    Instanciando objeto

---
+++ aula1_pyladies.py
@@ -0,0 +1,14 @@
+#Aula de Programação Orientada a objetos. Ministrada por Juliana Ferreira.
+
+#criação da classe
+class Pyladies:
+    def __init__(self):       #função init inicia quando o objeto é instanciado; conhecido como método construtor
+        print('Você criou seu objeto')
+
+#instanciando o objeto
+pyladies_mt = Pyladies()
+
+'''
+class Pyladies: #criação da classe
+    def __init__(self, nome_e, qtd_membros_e, tem_homens_e):        #e de entrada
+'''
