9ab98d534129c0ffd4e7897848ffe2a6534afb39
Marie Dias <mariane.gomes@fisica.ufmt.br>
2020-11-19 13:06:01 -0400

    Adicionando atributos

--- aula1_pyladies.py
+++ aula1_pyladies.py
@@ -1,14 +1,19 @@
-#Aula de Programação Orientada a objetos. Ministrada por Juliana Ferreira.
+#Aula de Programação Orientada a objetos. Ministrada por Juliana Ferreira, membra do PyLadies Brasil.
+#Adicionando atributos
 
 #criação da classe
 class Pyladies:
-    def __init__(self):       #função init inicia quando o objeto é instanciado; conhecido como método construtor
-        print('Você criou seu objeto')
+    def __init__(self, nome_e, qtd_membros_e,tem_homens_e):    #e de entrada
+        self.nome = nome_e #atributo nome recebe a variável nome_e
+        self.qtd_membros = qtd_membros_e
+        self.tem_homens = tem_homens_e
+        print("Atributos foram armazenados")
 
-#instanciando o objeto
-pyladies_mt = Pyladies()
+pyladies_mt = Pyladies("PyLadies Mato Grosso",6,False)
 
-'''
-class Pyladies: #criação da classe
-    def __init__(self, nome_e, qtd_membros_e, tem_homens_e):        #e de entrada
-'''
+print('\n', pyladies_mt.nome)
+print('\n',pyladies_mt.qtd_membros)
+print('\n',pyladies_mt.tem_homens)
+
+
+
