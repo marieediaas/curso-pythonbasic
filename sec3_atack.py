6cafcda42d2600809384138388eff5dc70922b0e
Marie Dias <mariane.gomes@fisica.ufmt.br>
2020-11-24 17:05:56 -0400

    importing modules from relative paths

--- sec3_atack.py
+++ sec3_atack.py
@@ -1,43 +1,4 @@
-import random
+from classes.enemy import Enemy
 
-class Enemy:
-    hp = 200
-    def __init__(self, atkl, atkh):
-        self.atk_low = atkl
-        self.atk_high = atkh
-
-    def getAtack(self):
-        print("Atack is ", self.atk_low)
-
-    def getHP(self):
-        print("Hp is ", self.hp)
-
-enemy1 = Enemy(40,49)
-enemy1.getAtack()
-enemy1.getHP()
-print("")
-enemy2 = Enemy(75,90)
-enemy2.getAtack()
-enemy2.getHP()
-
-
-'''
-player_hp = 260
-enem_atk_low = 60
-enem_atk_high = 80
-
-while player_hp > 0:
-    damage = random.randrange(enem_atk_low,enem_atk_high)
-    player_hp = player_hp - damage
-
-    if player_hp <= 30:
-        player_hp= 30
-
-    print("Enemy strikes for ", damage, "points of damage. Current HP is ", player_hp)
-
-    if player_hp > 30:
-        continue
-
-    print("You have low health, so you teleported to the nearest safe place.")
-    break
-'''
+enemy = Enemy(200,60)
+print("HP: ", enemy.get_hp())
