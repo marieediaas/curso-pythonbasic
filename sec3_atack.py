7ff60e51c7039102ef9d64cff2fe6aee99946633
Marie Dias <mariane.gomes@fisica.ufmt.br>
2020-11-24 15:39:04 -0400

    Teleport loop infinito

--- sec3_atack.py
+++ sec3_atack.py
@@ -9,10 +9,8 @@ while player_hp > 0:
     damage = random.randrange(enem_atk_low,enem_atk_high)
     player_hp = player_hp - damage
 
-    if player_hp <= 0:
-        player_hp= 0
+    if player_hp <= 30:
+        player_hp= 30
+        print("You have low health, so you teleported to the nearest safe place.")
 
     print("Enemy strikes for ", damage, "points of damage. Current HP is ", player_hp)
-
-    if player_hp == 0:
-        print("\nYou have died. You cannot respawn, as you are dead.")
