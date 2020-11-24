4893c129dd5f2910d331c148af00f9e7ab777650
Marie Dias <mariane.gomes@fisica.ufmt.br>
2020-11-24 15:19:37 -0400

    PEP guidelines

---
+++ sec3_pepguide.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+
+def my_function(one, two,
+                three, four,
+                five, six):
+    print("Hello World!")
+
+
+def second_function():
+    print("Second function")
+
+my_list = [1,2,
+           3,4,
+           5,6]
