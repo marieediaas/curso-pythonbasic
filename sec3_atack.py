d9fd54521f0255944d2cfe7dc5aff3573e030d28
Marie Dias <mariane.gomes@fisica.ufmt.br>
2020-11-24 15:34:57 -0400

    Breaking out of while loops

---
+++ sec3_atack.py
@@ -0,0 +1,18 @@
+import random
+
+
+player_hp = 260
+enem_atk_low = 60
+enem_atk_high = 80
+
+while player_hp > 0:
+    damage = random.randrange(enem_atk_low,enem_atk_high)
+    player_hp = player_hp - damage
+
+    if player_hp <= 0:
+        player_hp= 0
+
+    print("Enemy strikes for ", damage, "points of damage. Current HP is ", player_hp)
+
+    if player_hp == 0:
+        print("\nYou have died. You cannot respawn, as you are dead.")
