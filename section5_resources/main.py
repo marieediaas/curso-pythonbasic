25d7f2feb638d018e7fd179ed26f45cfb867d729
Marie Dias <mariane.gomes@fisica.ufmt.br>
2024-02-05 16:19:11 -0300

    json

--- section5_resources/main.py
+++ section5_resources/main.py
@@ -1,5 +1,25 @@
-newfile = open("newfile.txt", "w+")
+#newfile = open("newfile.txt", "w+")
 
-string = "This is the content that will be written to the text file."
+#string = "This is the content that will be written to the text file."
 
-newfile.write(string)
+#newfile.write(string)
+
+import json
+import os
+
+#check if the file exists and if it is not empty
+if os.path.isfile("./ages.json") and os.stat("./ages.json").st_size != 0:
+    old_file = open("./ages.json", "r+")
+    #load as python useful objetc (loads) and read its content
+    data = json.loads(old_file.read())
+    print("Current age is", data["age"], "------- adding a year.")
+    data["age"] = data["age"] + 1
+    print("New age is: ", data["age"])
+else:
+    old_file = open("./ages.json", "w+")
+    data = {"name":"Nick", "age": 27}
+    print("No file found, setting default age to", data["age"])
+
+old_file.seek(0)
+#convert back to a json object
+old_file.write(json.dumps(data))
